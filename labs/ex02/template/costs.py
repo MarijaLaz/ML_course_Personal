b2dd3c742d13ad016f8835f1446b919911c863e6
ch-epfl-287052 <ch-epfl-287052@noto.epfl.ch>
2020-10-01 14:07:27 +0000

    second lab done

--- labs/ex02/template/costs.py
+++ labs/ex02/template/costs.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
 """Function used to compute the loss."""
 
-def compute_loss(y, tx, w):
+def compute_loss_MSE(y, tx, w):
     """Calculate the loss.
 
     You can calculate the loss using mse or mae.
     """
-    # ***************************************************
-    # INSERT YOUR CODE HERE
-    # TODO: compute loss by MSE / MAE
-    # ***************************************************
-    raise NotImplementedError
+    return np.sum(np.square(y-tx@w))/(y.shape[0]*2)
+
+def compute_loss_MAE(y, tx, w):
+    """Calculate the loss.
+
+    You can calculate the loss using mse or mae.
+    """
+    return np.sum(abs(y-tx@w))/(y.shape[0])
